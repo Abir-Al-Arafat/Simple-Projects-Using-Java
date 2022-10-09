e120740bfba8fe78db0c70d4c4c33248fb55307b
Abir-Al-Arafat <antorabir@gmail.com>
2022-10-10 02:14:11 +0600

    keeping track of number of tries

--- NumberGuessingGame/src/GuessingGame.java
+++ NumberGuessingGame/src/GuessingGame.java
@@ -6,28 +6,49 @@ public class GuessingGame {
 	public static void main(String[] args) {
 		// object to take input
 		Scanner scan = new Scanner(System.in);
-		// generating a random number		
-		int randomNumber = (int)(Math.random()*100 + 1);
 		
-		System.out.println(randomNumber);
+//		System.out.println(randomNumber);
 		
-		//default guessed answer		
-		int guess = -1; 
+		// variable for play again loop		
+		String playAgain = "";
 		
-		//loop will run until the user guesses a correct number		
-		while(guess != randomNumber) {
-			System.out.println("Guess a number between 1-100: ");
+		
+		do {
+			//variable to keep track of the wrong inputs
+			byte count = 0;
 			
-			//taking input
-			guess = scan.nextInt();
+			// generating a random number (1-100)		
+			int randomNumber = (int)(Math.random()*100 + 1);
 			
-			if (guess < randomNumber) 
-				System.out.println(guess + " is too low. Try Again!");
-			else if (guess > randomNumber)
-				System.out.println(guess + " is too high. Try Again!");
-			else
-				System.out.println(guess + " is correct.");
-		}
+			//default guessed answer		
+			int guess = -1; 
+			
+			//loop will run until the user guesses a correct number		
+			while(guess != randomNumber) {
+				System.out.println("Guess a number between 1-100: ");
+				
+				//taking input
+				guess = scan.nextInt();
+				
+				//adding 1 everytime the user guesses
+				count += 1;
+				
+				if (guess < randomNumber) 
+					System.out.println(guess + " is too low. Try Again!");
+				else if (guess > randomNumber)
+					System.out.println(guess + " is too high. Try Again!");
+				else
+					System.out.println(guess + " is correct. \nit took you " + count + " tries");
+			}
+			
+			//asking if user want to play again			
+			System.out.println("Do you want to play again? (y/n): ");
+			//taking input			
+			playAgain = scan.next();
+			
+		}while(playAgain.equalsIgnoreCase("y"));
+		
+		System.out.println("GAME OVER!!");
 	}
 
 }
